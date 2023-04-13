10e69120148a855486818c41ab30b94c5d07eb20
Carina Cunha <carina.cunha13@gmail.com>
2023-04-13 16:07:53 -0300

    feat: implements scrape_news function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -6,9 +6,7 @@ from parsel import Selector
 def fetch(url):
     try:
         resp = requests.get(
-            url,
-            timeout=3,
-            headers={"user-agent": "Fake user-agent"}
+            url, timeout=3, headers={"user-agent": "Fake user-agent"}
         )
         time.sleep(1)
         if resp.status_code == 200:
@@ -37,7 +35,24 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    infos = {
+        "url": selector.css("link[rel='canonical']::attr(href)").get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("li.meta-author span.author a::text").get(),
+        "reading_time": int(
+            selector.css("li.meta-reading-time::text")
+            .get()
+            .split("minutos")[0]
+        ),
+        "summary": "".join(
+            selector.css("div.entry-content > p:nth-of-type(1)::text").getall()
+        ).strip(),
+        "category": selector.css(".meta-category a span.label::text").get(),
+    }
+
+    return infos
 
 
 # Requisito 5
