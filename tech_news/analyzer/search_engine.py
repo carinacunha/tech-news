498212a2ebbbaa6c7c77c4ff40ab48b7c5f3b3d6
Carina Cunha <carina.cunha13@gmail.com>
2023-04-13 17:30:24 -0300

    feat: implements search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    string = {"title": {"$regex": title, "$options": "i"}}
+    results = search_news(string)
+    if results:
+        return [(result["title"], result["url"]) for result in results]
+    else:
+        return []
 
 
 # Requisito 8
